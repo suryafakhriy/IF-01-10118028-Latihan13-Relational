e365be26d261f4385f1bdbf2516564e3034e3e3d
suryafakhriy <55834014+suryafakhriy@users.noreply.github.com>
2019-09-26 21:47:10 +0700

    Add files via upload

---
+++ IF-01-10118028-Latihan13-Relational/src/pkgif/pkg01/pkg10118028/latihan13/relational/IF0110118028Latihan13Relational.java
@@ -0,0 +1,34 @@
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pkgif.pkg01.pkg10118028.latihan13.relational;
+
+/**
+ *
+ * @author User
+ * NAMA      : Surya Fakhriy Hasbi
+   KELAS     : IF-01 2018
+   NIM       : 10118028
+   DESKRIPSI : Relational
+ */
+public class IF0110118028Latihan13Relational {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int bil1 = 10;
+        int bil2 = 20;
+        System.out.println("a == b = " + (bil1 == bil2));
+        System.out.println("a != b = " + (bil1 != bil2));
+        System.out.println("a > b = " + (bil1 > bil2));
+        System.out.println("a < b = " + (bil1 < bil2));
+        System.out.println("a >= b = " + (bil2 >= bil1));
+        System.out.println("a <= b = " + (bil2 <= bil1));
+    }
+    
+}
